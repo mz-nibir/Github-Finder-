7d3b0e947e5a705a4a99bafacfee5daddd5d8ad9
Nibir <mz.nibir@gmail.com>
2021-01-17 13:40:36 +0600

    UI class

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let searchBtn = document.querySelector("#searchBtn");
 let searchUser = document.querySelector("#searchUser");
+let ui= new UI();
 
 //add event listener
 searchBtn.addEventListener('click', (e) => {
