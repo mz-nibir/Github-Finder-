499c3d53b0700d4dea78af63740773e9c1a77402
Nibir <mz.nibir@gmail.com>
2021-01-17 12:21:53 +0600

    basic syntex of api

--- js/script.js
+++ js/script.js
@@ -6,7 +6,15 @@ searchBtn.addEventListener('click', (e) => {
     let userText = searchUser.value;
 
     if(userText != ""){
-        alert(userText);
+        //Fetch API
+        fetch(`https://api.github.com/users/${userText}`)
+        .then(result => result.json())
+        .then(data => {
+            console.log(data);
+
+
+        })
+
     }
 
 
